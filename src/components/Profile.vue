9650b7efe0b7c436a58d84392e8c69e9ec6b26d1
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-10-05 00:26:17 -0300

    configure styled components

---
+++ src/components/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <ProfileWrapper>
+    Teste
+  </ProfileWrapper>
+</template>
+
+<script>
+import styled from 'vue-styled-components';
+
+const ProfileWrapper = styled.div`
+    background-color: blue;
+`;
+
+export default {
+  name: 'Profile',
+  components: {
+    ProfileWrapper,
+  },
+};
+</script>
