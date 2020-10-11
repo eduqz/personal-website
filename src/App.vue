75ed3a6b7c9785c2512fc9fa971f43e3c2991ec6
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-10-11 01:17:16 -0300

    adjust sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
-  <div id="app">
+  <AppWrapper>
     <Sidebar/>
 
     <Profile/>
 
     <GlobalStyle/>
-  </div>
+  </AppWrapper>
 </template>
 
 <script>
+import styled from 'vue-styled-components';
 import Sidebar from './components/Sidebar.vue';
 import Profile from './components/Profile.vue';
 import { GlobalStyle } from './assets/style';
 
+const AppWrapper = styled.div`
+  padding: 0 0 0 5rem;
+`;
+
 export default {
   name: 'App',
   components: {
+    AppWrapper,
     Sidebar,
     Profile,
     GlobalStyle,
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,17 +1,24 @@
 <template>
   <SidebarWrapper>
-   <HightlightedText>EDUARDO ALMEIDA</HightlightedText>
+    <HightlightedText>EDUARDO ALMEIDA</HightlightedText>
 
-   <SocialMedias>
-       <GithubIcon />
-   </SocialMedias>
+    <SocialMedias>
+      <a href="www.google.com" target="_blank">
+        <Icon :src="require('../assets/icons/github.svg')" />
+      </a>
+      <a href="www.google.com" target="_blank">
+        <Icon :src="require('../assets/icons/github.svg')" />
+      </a>
+      <a href="www.google.com" target="_blank">
+        <Icon :src="require('../assets/icons/github.svg')" />
+      </a>
+    </SocialMedias>
   </SidebarWrapper>
 </template>
 
 <script>
 import styled from 'vue-styled-components';
 import { Theme } from '../assets/style';
-import GithubIcon from '../assets/icons/github.svg';
 
 const SidebarWrapper = styled.div`
   background-color: ${Theme.color.darkGray};
@@ -35,9 +42,19 @@ const HightlightedText = styled.p`
 `;
 
 const SocialMedias = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
-const Icon = styled.svg``;
+const Icon = styled.img`
+  margin-top: 1rem;
+  height: 1.5rem;
+  transition: all .2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
 
 export default {
   name: 'Sidebar',
@@ -46,7 +63,6 @@ export default {
     HightlightedText,
     SocialMedias,
     Icon,
-    GithubIcon,
   },
 };
 </script>
