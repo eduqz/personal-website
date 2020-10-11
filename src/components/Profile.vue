3392274665a62b784dec61db8557ab9642371c15
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-10-10 22:07:10 -0300

    create profile component

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,20 +1,128 @@
 <template>
   <ProfileWrapper>
-    Teste
+    <IntroductionWrapper>
+      <Subtitle>Me chamo Eduardo Almeida</Subtitle>
+      <Title>Entusiasta de Tecnologia</Title>
+      <Description>Fascinado por desenvolvimento de software</Description>
+
+      <TagsWrapper>
+        <Tag>Project Management</Tag>
+        <Tag>Development</Tag>
+      </TagsWrapper>
+    </IntroductionWrapper>
+
+    <PhotoWrapper>
+      <PhotoFrame>
+        <Photo :src="require('../assets/img/profile.jpeg')" alt=''/>
+      </PhotoFrame>
+    </PhotoWrapper>
+
+    <WaterMark>SOFTWARE</WaterMark>
   </ProfileWrapper>
 </template>
 
 <script>
 import styled from 'vue-styled-components';
+import PhotoURL from '../assets/img/profile.jpeg';
+import { Theme } from '../assets/style';
 
 const ProfileWrapper = styled.div`
-    background-color: blue;
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  background-color: #1B1B1B;
+  background-image: linear-gradient(to right, #1B1B1B , #3E3D3D);
+  display: grid;
+  padding: 4rem;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2rem;
+  align-items: center;
+`;
+
+const IntroductionWrapper = styled.div`
+  
+`;
+
+const Subtitle = styled.h2`
+  font: ${Theme.font.subtitle};
+  color: ${Theme.color.orange};
+  margin: 0;
+`;
+
+const Title = styled.h1`
+  font: ${Theme.font.title};
+  color: ${Theme.color.lightGray};
+  margin: 1.5rem 0 0 0;
+`;
+
+const Description = styled.h3`
+  font: ${Theme.font.bigText};
+  color: ${Theme.color.lightGray};
+  margin: 1rem 0 0 0;
+`;
+
+const TagsWrapper = styled.div`
+  display: flex;
+  margin-top: 2rem;
+`;
+
+const Tag = styled.p`
+  background-color: ${Theme.color.orange};
+  font: ${Theme.font.text};
+  font-weight: bold;
+  padding: 0.25rem 1rem;
+  border-radius: 2rem;
+  color: ${Theme.color.black};
+  margin: 0 1rem 0 0;
+`;
+
+const PhotoWrapper = styled.div``;
+
+const PhotoFrame = styled.div`
+  width: 80%;
+  height: 0;
+  padding: 40%;
+  border-radius: 50%;
+  border: solid 3px ${Theme.color.gray};
+  position: relative;
+  margin: auto;
+`;
+
+const Photo = styled.img`
+  width: calc(100% - 1rem);
+  height: calc(100% - 1rem);
+  border-radius: 50%;
+  border: solid 3px ${Theme.color.gray};
+  object-fit: cover;
+  position: absolute;
+  top: 0.5rem;
+  left: 0.5rem;
+`;
+
+const WaterMark = styled.p`
+  position: absolute;
+  bottom: 2rem;
+  left: 1rem;
+  font: ${Theme.font.waterMark};
+  color: ${Theme.color.lightGray};
+  opacity: 0.05;
 `;
 
 export default {
   name: 'Profile',
   components: {
     ProfileWrapper,
+    IntroductionWrapper,
+    Subtitle,
+    Title,
+    Description,
+    TagsWrapper,
+    Tag,
+    PhotoWrapper,
+    PhotoFrame,
+    Photo,
+    WaterMark,
+    PhotoURL,
   },
 };
 </script>
