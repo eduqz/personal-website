afc4fe8262595d53d200b3a143e7634431ab787a
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-10-11 17:22:39 -0300

    create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <FooterWrapper>
+    <EmailLink href="aaa">eduardoaqz@gmail.com</EmailLink>
+  </FooterWrapper>
+</template>
+
+<script>
+import styled from 'vue-styled-components';
+import { Theme } from '../assets/style';
+
+const FooterWrapper = styled.footer`
+  background-color: ${Theme.color.darkGray};
+  height: 4rem;
+  width: 100%;
+  padding: 0 4rem;
+  display: inline-flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const EmailLink = styled.a`
+  font: ${Theme.font.bigText};
+  color: ${Theme.color.lightGray};
+  cursor: pointer;
+  margin: auto 0;
+  transition: all .2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export default {
+  name: 'Footer',
+  components: {
+    FooterWrapper,
+    EmailLink,
+  },
+};
+</script>
